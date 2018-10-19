a0afa8d67b7f8ca9e76fefc9e1b1c637a6ff9506
tom <tmcdonagh12@gmail.com>
2018-10-19 16:14:27 -0500

    Cleaned out debug code and added comments

--- build_makemkv/pinger.sh
+++ build_makemkv/pinger.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Loops through different url combos to find newest makemkv version
 version=""
 keepRunning=true
 echo "-> Finding newest makemkv version..."
@@ -6,13 +7,14 @@ while :
 do
   for f in $(seq 1 10)
   do
-    for s in $(seq 12 100)
+    for s in $(seq 12 100) # Starts at 12 bc that was the newest version
     do
       for t in $(seq 1 10)
       do
-        #echo "$f.$s.$t"
+        #echo "$f.$s.$t" # Prints every version that's checked
         if [ $keepRunning == true ]
         then
+          # Checks to see if url exists
           curl -s --head http://www.makemkv.com/download/makemkv-oss-$f.$s.$t.tar.gz | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null
         else
           break
@@ -26,16 +28,10 @@ do
       done
     done
   done
-  curl -s --head http://www.makemkv.com/download/makemkv-oss-$version.tar.gz | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null
-  if [ $? == 0 ] 
-  then
-    break
-  elif [ $? == 1 ]
-  then
-    echo "One"
-  fi
 done
 echo "Version: $version"
+
+# Downloads tar files and removes version number from file name
 wget http://www.makemkv.com/download/makemkv-oss-$version.tar.gz
 mv makemkv-oss-$version.tar.gz /src/makemkv-oss.tar.gz
 wget http://www.makemkv.com/download/makemkv-bin-$version.tar.gz
